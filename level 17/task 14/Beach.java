6fd0da23c0544eb4a2d2acd2f67310142b9d16a3
Mafyak <Huba.est@gmail.com>
2017-10-18 15:57:41 -0400

    Create Beach.java

---
+++ level 17/task 14/Beach.java
@@ -0,0 +1,55 @@
+package com.javarush.task.task17.task1714;
+
+/* Comparable
+
+Реализуйте интерфейс Comparable<Beach> в классе Beach. Пляжи(Beach) будут использоваться нитями, поэтому позаботьтесь, чтобы все методы были синхронизированы.
+Реализовать метод compareTo так, чтобы он при сравнении двух пляжей выдавал число, которое показывает что первый пляж лучше(позитивное число)
+или второй пляж лучше (негативное число),и насколько он лучше.
+
+
+Требования:
+1. Класс Beach должен содержать три поля: String name, float distance, int quality.
+2. Класс Beach должен реализовывать интерфейс Comparable.
+3. Метод compareTo класса Beach как минимум должен учитывать качество пляжа и дистанцию.
+4. Все методы класса Beach, кроме метода main, должны быть синхронизированы.
+*/
+
+public class Beach {
+    private String name;      //название
+    private float distance;   //расстояние
+    private int quality;    //качество
+
+    public Beach(String name, float distance, int quality) {
+        this.name = name;
+        this.distance = distance;
+        this.quality = quality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
+    public void setDistance(float distance) {
+        this.distance = distance;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
