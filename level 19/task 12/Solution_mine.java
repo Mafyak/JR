7636a700e31871de3acfcb9596b61a3815b73e74
Mafyak <Huba.est@gmail.com>
2017-11-11 08:18:40 -0500

    Create Solution_mine.java

---
+++ level 19/task 12/Solution_mine.java
@@ -0,0 +1,51 @@
+package test;
+
+/* Ридер обертка 2
+В методе main подмените объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна заменять все подстроки «te» на «??«.
+Вызови готовый метод printSomething(), воспользуйтесь testString.
+Верни переменной System.out первоначальный поток.
+Выведи модифицированную строку в консоль.
+Требования:
+1. Класс Solution должен содержать класс TestString.
+2. Класс Solution должен содержать публичное статическое поле testString типа TestString, которое сразу проинициализировано.
+3. Класс TestString должен содержать публичный void метод printSomething().
+4. Метод printSomething() класса TestString должен выводить на экран строку "it's a text for testing".
+5. Метод main(String[] args) класса Solution должен создавать поток PrintStream (используй PrintStream c параметром конструктора ByteArrayOutputStream).
+6. Метод main(String[] args) класса Solution должен подменять и восстанавливать поток вывода в консоль объекта System.out
+7. Метод main(String[] args) класса Solution должен вызывать метод printSomething(),объекта testString.
+8. Метод main(String[] args) класса Solution должен модифицировать строку выведенную методом printSomething() согласно заданию, и выводить её в консоль.
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) throws IOException {
+
+        PrintStream tempPrintOutputer = System.out;
+
+        ByteArrayOutputStream workingStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(workingStream);
+        System.setOut(printStream);
+
+        testString.printSomething();
+        String result = workingStream.toString().replaceAll("te", "??");
+
+        System.setOut(tempPrintOutputer);
+
+        System.out.println(result);
+
+        workingStream.close();
+        printStream.close();
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's a text for testing");
+        }
+    }
+}
